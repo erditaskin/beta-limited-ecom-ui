0ffaa84d77b5865e2acf809caa8fb4477364ad30
Erdi TAŞKIN <erditaskin@gmail.com>
2023-11-22 03:50:09 +0300

    utils duplicate code cleanup

--- src/core/helpers/utils.js
+++ src/core/helpers/utils.js
@@ -1,6 +1,3 @@
-export const defineModule = (title, reducers, routes) => {
-  return { title, reducers, routes };
-};
 export const currencyFormat = (price) => {
   return "$" + price.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
 };
